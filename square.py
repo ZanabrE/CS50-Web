d3e0765da2db41cf503862750b4966fb156fec28
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-05-16 12:57:01 -0400

    imported files

--- square.py
+++ square.py
@@ -1,3 +1,7 @@
+# Call a functions from another file 
+# it needs to be imported.
+from functions import square
+
 for i in range(10):
     # f stands for formatted string literal - denotes the an f-string
     print(f'The square of {i} is {square(i)}')
