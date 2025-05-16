405218a5e3f744895e79bd4d9871b6b5b948133a
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-05-16 11:21:24 -0400

    modified: sequences.py

--- sequences.py
+++ sequences.py
@@ -1,4 +1,14 @@
-name = "Harry"
+#name = "Harry"
 
-print(name[1])
+# This line of code will give you the letter a from Harry.
+#print(name[1])
+
+# This is a list of data.
+names = ["Harry", "Ron", "Hermione"]
+
+# This will print all the names.
+#print(names)
+
+# This will print the first item of the list.
+print(names[0])
 
