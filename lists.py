5f5a96e4a97b2ea1bd175c0c0593d66bbe44c684
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-05-16 12:07:14 -0400

    modified: files

--- lists.py
+++ lists.py
@@ -1,8 +1,17 @@
 # Define a list of names
 names = ["Alice", "Bob", "Charlie", "David", "Eve"]
 
+# Adding a name
 names.append("Draco")
 
+# Sorting a name
 names.sort()
 
+# Printing the list
+print(names)
+
+# Removing a name
+names.remove("Bob")
+
+# Printing the list
 print(names)
