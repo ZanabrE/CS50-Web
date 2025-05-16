e35f9ad553a11e0ba9943e60559f0902407fbb43
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-05-16 11:26:43 -0400

    modified: sequences.py

--- sequences.py
+++ sequences.py
@@ -4,11 +4,19 @@
 #print(name[1])
 
 # This is a list of data.
-names = ["Harry", "Ron", "Hermione"]
+#names = ["Harry", "Ron", "Hermione"]
 
 # This will print all the names.
 #print(names)
 
 # This will print the first item of the list.
-print(names[0])
+#print(names[0])
 
+#Tuples
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+
+print(coordinate[0])
+print(coordinate[1])
