f8448cb33e224f758110c2fc06efa8d85bec6d37
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-05-16 10:55:56 -0400

    modified: conditions.py

--- conditions.py
+++ conditions.py
@@ -1,3 +1,12 @@
+# This line of code will give you an error 
+# because the input function takes user input 
+# as string.
+# n = input("Number: ")
+
+# Adding the int (integer) infront of the input
+# ensures that the value entered is treated as a 
+# number.
+
 n = int(input("Number: "))
 
 if n > 0:
