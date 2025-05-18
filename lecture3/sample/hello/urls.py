b6eb7c8e48b287bf582e4682c6f6165a96f13be1
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-05-18 15:33:41 -0400

    modified: hello/urls.py

--- lecture3/sample/hello/urls.py
+++ lecture3/sample/hello/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:name>", views.desconocido, name="desconocido"),
+    path("greet", views.greet, name="greet"),
+    path("saludos", views.saludos, name="saludos")
 ]
