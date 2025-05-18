587c8a1ab055627df07d6325f00b5b4ddc29fca2
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-05-18 15:34:28 -0400

    modified: hello/views.py

--- lecture3/sample/hello/views.py
+++ lecture3/sample/hello/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    return render(request, "hello/index.html")
+
+def brian(request):
     return HttpResponse("Hello, World!")
+
+def greet(request):
+    return HttpResponse("Hello, Ernesto!")
+
+def saludos(request):
+    return HttpResponse("Saludos, Ernesto!")
+
+def desconocido(request, name):
+    return HttpResponse(f"Hola, {name.capitalize()}!")
