76a47ae2ffed30e68c33ca1c00a6d1f7dbf92bf0
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-07-05 14:25:33 -0400

    deleted files

--- dictionaries.py
+++
@@ -1,8 +0,0 @@
-# A dictionary is a set of key-value pairs,
-# where each key has a corresponding value.
-
-houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
-
-print(houses["Harry"])
-print(houses["Draco"])
-
--- functions.py
+++
@@ -1,7 +0,0 @@
-# Def as define the function.
-def square(x):
-    return x * x
-
-#for i in range(10):
-    # f stands for formatted string literal - denotes the an f-string
-    #print(f'The square of {i} is {square(i)}')
