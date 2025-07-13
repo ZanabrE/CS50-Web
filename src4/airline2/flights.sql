8c2f8570fc1553da968b297abe95cfa77751d590
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-07-13 12:27:16 -0400

    modified file

--- src4/airline2/flights.sql
+++ src4/airline2/flights.sql
@@ -1,12 +1,9 @@
 -- SQLBook: Code
 CREATE TABLE flights (
-    flight_id INT PRIMARY KEY,
-    flight_number VARCHAR(10) NOT NULL,
-    departure_time DATETIME NOT NULL,
-    arrival_time DATETIME NOT NULL,
-    origin VARCHAR(100) NOT NULL,
-    destination VARCHAR(100) NOT NULL,
-    airline VARCHAR(50) NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    origin TEXT NOT NULL,
+    destination TEXT NOT NULL,
+    duration INTEGER NOT NULL
 );
 
 
