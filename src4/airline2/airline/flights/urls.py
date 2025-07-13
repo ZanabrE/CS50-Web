52779a6bf1e72d0086c83792808cb3053c069b01
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-07-13 13:36:58 -0400

    modified and added new files

--- src4/airline2/airline/flights/urls.py
+++ src4/airline2/airline/flights/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name = 'flights'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:flight_id>/', views.detail, name='detail'),
-    path('<int:flight_id>/book/', views.book, name='book'),
+    
 ]
