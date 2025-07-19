d0ebe604ccdc12ccbb1d219742db39fd594dbd3f
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-07-19 12:45:47 -0400

    modified files

--- src4/airline2/airline/flights/models.py
+++ src4/airline2/airline/flights/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 
 # Create your models here.
-
-# Airpot table in models.py
+# Airline table in models.py
 class Airport(models.Model):
     code = models.CharField(max_length=3)
     city = models.CharField(max_length=64)
 
     def __str__(self):
         return f"{self.city}, ({self.code})"
-
-
+    
+    
 # Flight table in models.py
 class Flight(models.Model):
     origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
@@ -19,4 +18,4 @@ class Flight(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination} ({self.duration} minutes)"
-    
+
