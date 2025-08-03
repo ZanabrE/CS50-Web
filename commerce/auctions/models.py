5073f3b7fe0a5260d348962b404da573a30e08e2
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-08-03 13:53:40 -0400

    new files and modified file

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image_url = models.URLField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    isActive = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+
